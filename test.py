32fc533ea569017b784db3af2d7fe68e3c9fde36
Starstorm123 <118751756+Starstorm123@users.noreply.github.com>
2023-09-25 13:51:45 +0530

    Update test.py

--- test.py
+++ test.py
@@ -3,7 +3,7 @@ import pandas_datareader as web
 import numpy as np
 import pandas as pd
 import yfinance as yf
-from scikit-learn.preprocessing import MinMaxScaler
+from sklearn.preprocessing import MinMaxScaler
 from keras.models import Sequential
 from keras.layers import Dense,LSTM
 import matplotlib.pyplot as plt
